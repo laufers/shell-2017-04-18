1de871266eda401e2f8b532d9349b420c9092886
Evan Linde <evanlinde@gmail.com>
2017-03-30 14:46:10 -0500

    quote filenames in tests

--- make_html.sh
+++ make_html.sh
@@ -8,7 +8,7 @@ ANSI_DIR="full_terminal.d"
 ANSI2HTML_OPTIONS="--bg=dark"
 
 ANSI_DIR_NEWEST=""  # newest file in ${ANSI_DIR}
-if [ -d ${ANSI_DIR} ]; then
+if [ -d "${ANSI_DIR}" ]; then
   ANSI_DIR_NEWEST="$(ls -1t ${ANSI_DIR}/[0-9][0-9][0-9] | head -1)"
 fi
 
@@ -17,14 +17,14 @@ fi
 
 function do_file {
   # convert single file to html
-  if [ ${ANSI} -nt ${HTML} ]; then 
+  if [ "${ANSI}" -nt "${HTML}" ]; then 
     cat ${ANSI} | ./ansi2html.sh ${ANSI2HTML_OPTIONS} > ${HTML}
   fi
 }
 
 function do_dir {
   # convert files in directory full_terminal.d to html
-  if [ ${ANSI_DIR_NEWEST} -nt ${HTML} ]; then
+  if [ "${ANSI_DIR_NEWEST}" -nt "${HTML}" ]; then
     # files are named numerically with leading zeros 
     # so they should glob in the correct order
     cat ${ANSI_DIR}/* | ./ansi2html.sh ${ANSI2HTML_OPTIONS} > ${HTML}
@@ -40,10 +40,10 @@ if [ ! -x ansi2html.sh ]; then
   chmod +x ansi2html.sh
 fi
 
-if [ ${ANSI} -nt ${ANSI_DIR_NEWEST} ]; then
+if [ "${ANSI}" -nt "${ANSI_DIR_NEWEST}" ]; then
   # must be using script, convert single file
   do_file
-elif [ ${ANSI_DIR_NEWEST} -nt ${ANSI} ]; then
+elif [ "${ANSI_DIR_NEWEST}" -nt "${ANSI}" ]; then
   # must be using mintty log, convert directory
   do_dir
 else
